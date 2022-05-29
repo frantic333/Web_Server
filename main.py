c908b9cd69a94da4c172f84efb909813639bb537
frantic333 <71868384+frantic333@users.noreply.github.com>
2022-05-29 23:44:56 +0300

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
- # This is a sample Python script.
+import socket
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def start_my_server():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 2000))
+        #Можно так
+        #server = socket.create_server(('127.0.0.1', 2000))
 
+        # Указание серверу слушать указанный порт
+        server.listen(4)
+        while True:
+            print('Working...')
+            #С помощью метода accept сервер принимает отправленные ему запросы и
+            # разделяет их на клиента и адрес, с которого прилетел запрос.
+            #Особенностью этого метода является то, что на этом моменте программа
+            # фиксируется и не идет дальше до тех пор пока кто- то не подключится
+            # к этому серверу
+            client_socket, address = server.accept()
+            #Из оъекта клиента получаем содержимое запроса
+            data = client_socket.recv(1024).decode('utf-8')
+        #    print(data)
+            #На отправку клиенту
+            content = load_page_from_get_request(data)
+            #Энкодируем заголовки и добавляем наш контент
+            client_socket.send(content)
+            #Закрытие соединения с конкретным клиентом после того, как ему был отправлен ответ
+            client_socket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('shutdown this shut...')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Функция обработчик
+def load_page_from_get_request(request_data):
+    # Указание браузеру Chrome напрямую, что передается именно html информация.
+    # Это делается с помощью заголовков, в котором указывается тип ответа, статус,
+    # код статуса и кодировка
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    #Заголовок если страница не найдена 404
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    #Парсинг запроса клиента
+    path = request_data.split(' ')[1]
+    response = ''
+    try:
+        with open('views'+path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + 'Sorry, bro! No page...').encode('utf-8')
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    start_my_server()
